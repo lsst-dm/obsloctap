fcf8f5671940277e42a43594a0b7336be09a3ac7
William O'Mullane <womullan@lsst.org>
2025-04-07 11:27:13 -0400

    with gcc in consumekafka because rubin-sim needs it, fix for async tests

--- tests/ScheduleTest.py
+++ tests/ScheduleTest.py
@@ -7,7 +7,9 @@ from obsloctap.schedule24h import Schedule24
 
 class TestSchedule(unittest.TestCase):
     def test_schedule_format(self) -> None:
-        visits = pd.read_pickle("tests/schedule24.pkl")
+        # schedule24.pkl was direclty from sqlite files
+        # schedule24rs.pkl is from the api call (see testutils)
+        visits = pd.read_pickle("tests/schedule24rs.pkl")
         obs = Schedule24().format_schedule(visits)
         self.assertEqual(len(obs), len(visits), "Not all entries")
         self.assertGreaterEqual(len(obs), 10, "Not enough entries")

5796e362ab0a47d5b4d265f834003cb8e410926a
William O'Mullane <womullan@lsst.org>
2025-08-01 12:28:52 +0100

    headers on all python

--- src/obsloctap/consumekafka.py
+++ src/obsloctap/consumekafka.py
@@ -1,3 +1,14 @@
+# This file is part of obsloctap.
+#
+# Developed for the Rubin Data Management System.
+# This product includes software developed by the Rubin Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
 """Copied from consdb -and perhaps they could be combined in the future"""
 
 import asyncio

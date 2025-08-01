e766f65a34ab191d150272a718b5d085c49ee910
William O'Mullane <womullan@lsst.org>
2025-08-01 12:28:52 +0100

    headers on all python

--- src/obsloctap/__init__.py
+++ src/obsloctap/__init__.py
@@ -1,3 +1,14 @@
+# This file is part of obsloctap.
+#
+# Developed for the Rubin Data Management System.
+# This product includes software developed by the Rubin Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
 """The obsloctap service."""
 
 __all__ = ["__version__"]
--- src/obsloctap/handlers/internal.py
+++ src/obsloctap/handlers/internal.py
@@ -1,3 +1,14 @@
+# This file is part of obsloctap.
+#
+# Developed for the Rubin Data Management System.
+# This product includes software developed by the Rubin Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
 """Internal HTTP handlers that serve relative to the root path, ``/``.
 
 These handlers aren't externally visible since the app is available at a path,
